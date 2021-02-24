ad3c3d7767dcbd0aac8284919366cede6ebd5114
Irina180586 <64046541+Irina180586@users.noreply.github.com>
2021-02-24 09:13:30 -0500

    commit for feedback

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.log("Your index.js file is loaded correctly!");
+
+
+$("#Skillset").on("click", function(){
+    $("#myModal").show();
+});
+
+$("#close").on("click", function(){
+    $("#myModal").hide();
+});
