62ddb53d5365def5f879a47cdd82560b387f8e3c
Irina180586 <64046541+Irina180586@users.noreply.github.com>
2021-03-03 23:38:18 -0500

    fixed back to top

--- js/index.js
+++ js/index.js
@@ -11,25 +11,25 @@ $("#close").on("click", function(){
 
 
 
-// $(document).ready(function () {
-//     if ($(window).scrollTop() > 145) {
-//         $("#navbar").css('opacity', '100');
-//     }
-//     $(document).on('scroll', function () {
-//         if ($(window).scrollTop() < 145) {
-//             $("#navbar").css('opacity', '0');
-//             if($(window).scrollTop() < 500){
-//                 console.log('ther');
-//             $('#back-to-top').fadeOut("slow");
-//             }
-//         }
-//         if ($(window).scrollTop() > 145) {
-//             $("#navbar").css('opacity', '100');
-//             if ($(window).scrollTop() > 500) {
-//                 console.log('ere');
-//                 // $('#back-to-top').fadeIn("slow");
-//             }
-//         }
-//     });
-// });
+$(document).ready(function () {
+    if ($(window).scrollTop() > 145) {
+        $("#navbar").css('opacity', '100');
+    }
+    $(document).on('scroll', function () {
+        if ($(window).scrollTop() < 145) {
+            $("#navbar").css('opacity', '0');
+          
+                // console.log('ther');
+            $('.back-to-top').css('opacity', '0');
+            
+        }
+        if ($(window).scrollTop() > 145) {
+            console.log("here");
+            $("#navbar").css('opacity', '100');
+            $('.back-to-top').css('opacity', '100');
+
+            
+        }
+    });
+});
 
