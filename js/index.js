2cfcf79cc9300f2af45b2e0d3fe1fc51d57dc885
Irina180586 <64046541+Irina180586@users.noreply.github.com>
2021-03-03 23:01:56 -0500

    Push with bootstrap

--- js/index.js
+++ js/index.js
@@ -8,3 +8,28 @@ $("#Skillset").on("click", function(){
 $("#close").on("click", function(){
     $("#myModal").hide();
 });
+
+
+
+// $(document).ready(function () {
+//     if ($(window).scrollTop() > 145) {
+//         $("#navbar").css('opacity', '100');
+//     }
+//     $(document).on('scroll', function () {
+//         if ($(window).scrollTop() < 145) {
+//             $("#navbar").css('opacity', '0');
+//             if($(window).scrollTop() < 500){
+//                 console.log('ther');
+//             $('#back-to-top').fadeOut("slow");
+//             }
+//         }
+//         if ($(window).scrollTop() > 145) {
+//             $("#navbar").css('opacity', '100');
+//             if ($(window).scrollTop() > 500) {
+//                 console.log('ere');
+//                 // $('#back-to-top').fadeIn("slow");
+//             }
+//         }
+//     });
+// });
+
